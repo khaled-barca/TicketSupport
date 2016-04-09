c8ad7cb3806935e5349ebfdd427cd0df9c4e7b83
ayahmaged <ayahmaged0@gmail.com>
2016-04-09 02:33:56 +0200

    trial only

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class TestController extends Controller
+{
+    
+    public function test(\App\Ticket $ticket, Illuminate\Http\Request $request){
+        
+               
+               $ticket->update([
+                   'support_id' => $request->id
+               ]);
+       
+               
+               return view('success');
+    }
+    
+    
+}
